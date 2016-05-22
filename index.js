5243ce07a0d7db743e6e26ce86193240ef1d8f03
benminton <coolmanfrancis@gmail.com>
2016-05-22 10:58:16 +0800

    Add MongoDB

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://ben:ben@ds011893.mlab.com:11893/heroku_d391lxtw',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '2W5Bt9Aky3',
   masterKey: process.env.MASTER_KEY || 'yQhdZfwoTO', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://easysuccess-parse.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
